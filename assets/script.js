7c34a6b74ae74008f435f6fa496e27ce691de46d
p-hyun <amabile___@naver.com>
2022-10-24 01:36:48 +0900

    사진추가

--- assets/script.js
+++ assets/script.js
@@ -16,24 +16,24 @@ const swiper = new Swiper('.swiper', {
   });
 
 
-var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
-var options = { //지도를 생성할 때 필요한 기본 옵션
-  center: new kakao.maps.LatLng(37.2617874, 127.0334682), //지도의 중심좌표.
-  level: 4 //지도의 레벨(확대, 축소 정도)
-};
+// var container = document.getElementById('map'); //지도를 담을 영역의 DOM 레퍼런스
+// var options = { //지도를 생성할 때 필요한 기본 옵션
+//   center: new kakao.maps.LatLng(37.2617874, 127.0334682), //지도의 중심좌표.
+//   level: 4 //지도의 레벨(확대, 축소 정도)
+// };
   
   
-var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
+// var map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
 
-var markerPosition  = new kakao.maps.LatLng(37.2617874, 127.0334682); 
+// var markerPosition  = new kakao.maps.LatLng(37.2617874, 127.0334682); 
 
-// 마커를 생성합니다
-var marker = new kakao.maps.Marker({
-    position: markerPosition
-});
+// // 마커를 생성합니다
+// var marker = new kakao.maps.Marker({
+//     position: markerPosition
+// });
 
-// 마커가 지도 위에 표시되도록 설정합니다
-marker.setMap(map);
+// // 마커가 지도 위에 표시되도록 설정합니다
+// marker.setMap(map);
 
 //modal
 const modal = document.querySelector('.modal');
