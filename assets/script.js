1412548df4cefab673470c46e252acac4f93a00c
p-hyun <amabile___@naver.com>
2022-10-24 13:06:29 +0900

    팝업 오픈 설정

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,56 @@
+let pictureArr = [
+  "../img/1.jpg",
+  "../img/2.jpg",
+  "../img/3.jpg",
+  "../img/4.jpg",
+  "../img/5.jpg",
+  "../img/6.jpg",
+  "../img/7.jpg",
+  "../img/8.jpg",
+  "../img/9.jpg",
+  "../img/10.jpg",
+  "../img/11.jpg",
+  "../img/12.jpg",
+  "../img/13.jpg",
+  "../img/14.jpg",
+  "../img/15.jpg",
+  "../img/16.jpg",
+  "../img/17.jpg",
+  "../img/18.jpg",
+  "../img/19.jpg",
+  "../img/20.jpg",
+  "../img/21.jpg",
+  "../img/22.jpg",
+  "../img/23.jpg",
+  "../img/24.jpg",
+  "../img/25.jpg",
+  "../img/26.jpg",
+  "../img/27.jpg",
+  "../img/28.jpg",
+  "../img/29.jpg",
+  "../img/0.jpg",
+];
+
+document.addEventListener("DOMContentLoaded", function(){
+  pictureArr.forEach(function(src) {
+    console.log(src);
+    let str =  `<div class="swiper-slide swiper-img"><img class="imgSrc" src=`+ src +`></div>`;
+     $(".wrapSwiper").append(str);
+    let popStr = `<div class="swiper-slide popSwiperImg"><img class="imgSrc" src=`+ src +`></div>`;
+    $(".wrapSwiperPop").append(popStr);
+  });
+
+  $(".imgSrc").on("click", function(e) {
+    let img = e.target;
+    let src = $(img).attr("src");
+    let idx = pictureArr.findIndex(e => e == src);
+    alert(idx);
+    swiper.slideTo(idx,200,false);
+    fn_layer('layer01', 600);
+  })
+  
+});
+
 const swiper = new Swiper('.swiper', {
     lazy: {
         loadPrevNext: true,
@@ -13,6 +66,24 @@ const swiper = new Swiper('.swiper', {
       prevEl: '.swiper-button-prev',
     },
   
+  });
+
+  new Swiper('.swiper-container', {
+
+    slidesPerView : 3, // 동시에 보여줄 슬라이드 갯수
+    spaceBetween : 30, // 슬라이드간 간격
+    slidesPerGroup : 3, // 그룹으로 묶을 수, slidesPerView 와 같은 값을 지정하는게 좋음
+  
+    // 그룹수가 맞지 않을 경우 빈칸으로 메우기
+    // 3개가 나와야 되는데 1개만 있다면 2개는 빈칸으로 채워서 3개를 만듬
+    loopFillGroupWithBlank : true,
+  
+    loop : true, // 무한 반복
+
+    navigation : { // 네비게이션
+      nextEl : '.swiper-button-next', // 다음 버튼 클래스명
+      prevEl : '.swiper-button-prev', // 이번 버튼 클래스명
+    },
   });
 
 
@@ -35,8 +106,8 @@ const swiper = new Swiper('.swiper', {
 // // 마커가 지도 위에 표시되도록 설정합니다
 // marker.setMap(map);
 
+
 //modal
-const modal = document.querySelector('.modal');
 const btnOpenPopup = document.querySelector('.btn-open-popup');
 
 const layerPop = document.querySelector('.layerPop');
@@ -46,11 +117,6 @@ btnOpenPopup.addEventListener('click', () => {
   fn_layer('layer01', 600);
 });
 
-layerPop.addEventListener('click', (e) => {
-  if(!$(e.target).is('.layer01 .container')) {
-    $(".layer01").fadeOut(200).removeClass("on");
-    $("body, html").removeAttr('style');}
-});
 
 // 레이어팝업
 function fn_layer(e,s) {
