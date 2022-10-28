b17ab27abcabe90d5b041cefd442e0e89e323309
p-hyun <amabile___@naver.com>
2022-10-28 13:16:23 +0900

    터치 막기

--- assets/script.js
+++ assets/script.js
@@ -127,8 +127,12 @@ var marker = new kakao.maps.Marker({
 // 마커가 지도 위에 표시되도록 설정합니다
 marker.setMap(map);
 
+function setDraggable(draggable) {
+  // 마우스 드래그로 지도 이동 가능여부를 설정합니다
+  map.setDraggable();    
+}
 
-modal
+
 
 const layerPop = document.querySelector('.layerPop');
 
